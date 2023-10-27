3f2ad3e89f325af163ca792df83c0354995cdf07
Love <ldhameli@uncc.edu>
2023-10-27 13:22:01 -0400

    Final Commit

--- tests/e2e/test_create_movies_page.py
+++ tests/e2e/test_create_movies_page.py
@@ -1,18 +1,56 @@
 # TODO: Feature 2
 import pytest
-from app import app
+from app import app, movie_repository
 
-@pytest.fixture(scope='module')
-def test_client():
-    with app.test_client() as client:
-        yield client
 
-def test_access_create_movies_page(test_client):
-    # Make a GET request to the create movies page
-    response = test_client.get('/movies/new')
 
-    # Assert the status code
+def test_display_create_movie_form(test_app):
+    """Test if the create movie form is displayed correctly."""
+    response = test_app.get("/movies/new")
+    data = response.data.decode('utf-8')
+    
     assert response.status_code == 200
-
-    # Assert that the response contains the expected text
+   
     assert b"Create Movie Rating" in response.data
+
+def test_submit_create_movie_form(test_app):
+
+
+    """Test submitting the create movie form."""
+    endClear = movie_repository.clear_db()
+    endClear = movie_repository.get_all_movies()
+    #checking number of movies
+    initial_movies_count = len(endClear)
+    
+    # Simulating a form submission
+    response = test_app.post('/movies', data={
+        'title': 'Avengers',
+        'director': 'Test Director',
+        'rating': 5
+    })
+
+    #redirect to movie page
+    assert response.status_code == 302
+    
+    endClear = movie_repository.get_all_movies()
+    #checking cout increase
+    final_movies_count = len(endClear)
+    assert final_movies_count == initial_movies_count + 1
+
+    response = test_app.get("/movies")
+    data = response.data.decode('utf-8')
+    
+    assert response.status_code == 200
+    assert "Avengers" in data
+
+    assert "Ironman" not in data
+    
+    response = test_app.post('/movies', data={
+        'title': 'Thor',
+        'director': 'Chris',
+        'rating': 5
+    })
+    
+    response = test_app.get("/movies")
+    data = response.data.decode('utf-8')
+    assert "Thor" in data
--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,41 +1,30 @@
 # TODO: Feature 2
 import pytest
-from app import app, movie_repository
+from app import app,  movie_repository
 
 @pytest.fixture()
 def test_app():
     return app.test_client()
 
-def test_display_create_movie_form(test_app):
-    """Test if the create movie form is displayed correctly."""
-    response = test_app.get("/movies/new")
-    
-    assert response.status_code == 200
-    # Assuming you have some distinctive phrases in your form, like:
-    assert b"Create Movie Rating" in response.data
+def test_create_movie_empty(test_app):
+    response = test_app.get('/movies')
+    data = response.data.decode('utf-8')
 
-def test_submit_create_movie_form(test_app):
-    """Test submitting the create movie form."""
-    
-    # Before submission, let's check the current number of movies
-    initial_movies_count = len(movie_repository.get_all_movies())
-    
-    # Simulating a form submission
-    response = test_app.post('/movies', data={
-        'title': 'Test Movie',
-        'director': 'Test Director',
-        'rating': 5
-    })
+    Repo = movie_repository.clear_db()
+    assert Repo is None
+    Repo = movie_repository.get_all_movies()
+    assert "Avengers" not in Repo
 
-    # Expecting a redirect to the movie list page
-    assert response.status_code == 302
-    assert 'Location' in response.headers
-    assert response.headers['Location'].endswith('/movies')
+
+def test_create_movies_page(test_app):
+
+    movie = movie_repository.create_movie("Avengers", "Anthony", 2)
+    target = movie_repository.get_movie_by_title("Avengers")
+    Repo = movie_repository.get_all_movies()
+    assert movie.title == target.title
+    assert Repo is not None
+
+
+
     
-    # Check if the movie count increased by 1 after the form submission
-    final_movies_count = len(movie_repository.get_all_movies())
-    assert final_movies_count == initial_movies_count + 1
-
-    # Optional: Check if the new movie is present in the repository (this would need more repository functions)
-
-# You can add more tests for edge cases, incorrect form inputs, etc.
+    
